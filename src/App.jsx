5f9e978f649d218329267903473591b933ada89d
Yulevski <yonaiyuu@gmail.com>
2023-07-12 21:00:25 +0900

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import Mainpage from './components/Mainpage';
 import Photopage from './components/Photopage';
 import UIpage from './components/UIpage';
 import Apppage from './components/Apppage';
+import Designpage from './components/Designpage';
+import Blogpage from './components/Blogpage';
+import Illustpage from './components/Illustpage';
+import Photopage2 from './components/Photopage2';
+import Architectures from './components/Architectures';
+import Nature from './components/Nature';
+import Street from './components/Street';
+import Airplanes from './components/Airplanes';
+
 import './App.css';
 
 function App() {
@@ -27,10 +36,19 @@ function App() {
 
 
      <Switch>
-     <Route exact path="/main" render={() => <Mainpage/>} />
+     <Route exact path="/" render={() => <Mainpage/>} />
      <Route exact path="/photopage" render={() => <Photopage/>} />
      <Route exact path="/uipage" render={() => <UIpage/>} />
+     <Route exact path="/designpage" render={() => <Designpage/>} />
      <Route exact path="/apppage" render={() => <Apppage/>} />
+     <Route exact path="/blogpage" render={() => <Blogpage/>} />
+     <Route exact path="/illustpage" render={() => <Illustpage/>} />
+     <Route exact path="/photopage2" render={() => <Photopage2/>} />
+     <Route exact path="/architectures" render={() => <Architectures/>} />
+     <Route exact path="/nature" render={() => <Nature/>} />
+     <Route exact path="/street" render={() => <Street/>} />
+     <Route exact path="/airplanes" render={() => <Airplanes/>} />
+     {/* <Route exact path="/photopage2" render={() => <Photopage2/>} /> */}
      </Switch>
     
     </BrowserRouter>
--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {  Link , NavLink} from 'react-router-dom';
-import './styles/mainpage.css';
+import './styles/mainpage2.css';
 
 
 function Mainpage() {
@@ -12,25 +12,57 @@ function Mainpage() {
       <div class="wrapper-site-header_wrapper">
       <h1 className='title-app'>Yu's private works 18+</h1>
           <ul class="nav_wrapper">
-            <li class="nav_item"><NavLink to="/main">Main</NavLink></li>
-            <li class="nav_item"><NavLink to="/main">Main</NavLink></li>
-            <li class="nav_item"><NavLink to="/main">Main</NavLink></li>
-            <li class="nav_item"><NavLink to="/main">Main</NavLink></li>
-            <li class="nav_item"><NavLink to="/main">Main</NavLink></li>
+            <li class="nav_item"><NavLink to="/main">Home</NavLink></li>
+            <li class="nav_item"><a href="https://www.instagram.com/yu4nai/" target="_blank" rel="noreferrer">Instagram</a></li>
+            <li class="nav_item"><a href="https://www.linkedin.com/in/yu-yonai-02b82b15a/" target="_blank" rel="noreferrer">Linkedin</a></li>
+            <li class="nav_item"><a href="https://www.behance.net/b4436605" target="_blank" rel="noreferrer">Behance</a></li>
+            
           </ul>
       </div>
     </header>
-    
-    <div className="mainpage-container">      
+
+    <div className="mainpage-container"> 
+      <div className='photo-page-main'>
         <Link className='link' to="/Photopage">
-        <button className='photo-page-main'>Photo</button>
+        <button className='button-main'>Photo</button>
+        <h4 className='button-title-main'>Photography</h4>
         </Link>
+      </div>     
+        
+      <div className='ui-page-main'>
         <Link className='link' to="/uipage">
-        <button className='ui-page-main'>UI</button>
+        <button className='button-main'>UI</button>
+        <h4 className='button-title-main'>UI/UX Design</h4>
         </Link>
+      </div>
+
+        <div className='design-page-main'>
+        <Link className='link' to="/designpage">
+        <button className='button-main'>DE</button>
+        <h4 className='button-title-main'>Industrial Design</h4>
+        </Link>
+        </div>
+        
+        <div className='illustration-page-main'>
+        <Link className='link' to="/blogpage">
+        <button className='button-main'>Blog</button>
+        </Link>
+        <h4 className='button-title-main'>Blog</h4>
+        </div>
+
+        <div className='blog-page-main'>
         <Link className='link' to="/apppage">
-        <button className='app-page-main'>app</button>
+        <button className='button-main'>WebApp</button>
+        <h4 className='button-title-main'>WebApp+Semiotics</h4>
         </Link>
+        </div>
+
+        <div className='app-page-main'>
+        <Link className='link' to="/illustpage">
+        <button className='button-main'>Illustration</button>
+        <h4 className='button-title-main'>illustration</h4>
+        </Link>
+        </div>
       
       </div>{/* "mainpage-container" */}
 
